c04392802ccfc2fc8a48d632d3ad272589a24786
zygar <a@andywilkinson.co.nz>
2020-08-24 12:00:17 +0100

    adds samba configuration

--- symlink.sh
+++ symlink.sh
@@ -6,8 +6,8 @@ echo ${BASEDIR}
 
 sudo ln -s ${BASEDIR}/motd.d/motd /etc/motd
 sudo ln -s ${BASEDIR}/motd.d/update-motd.d /etc/update-motd.d
-# sudo ln -s ${BASEDIR}
+# sudo ln -s ${BASEDIR}/
 sudo ln -s ${BASEDIR}/ssh/authorized_keys ~/.ssh/authorized_keys
 sudo ln -s ${BASEDIR}/shell/.zshrc ~/.zshrc
 sudo ln -s ${BASEDIR}/shell/.zsh_aliases ~/.zsh_aliases
-
+sudo ln -s ${BASEDIR}/samba/smb.conf /etc/samba/smb.conf
